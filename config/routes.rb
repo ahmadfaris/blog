977779691c6067b80f4afa080dab69c0c5a623ce
ahmadfaris <faris91@gmail.com>
2015-07-05 13:01:49 +0800

    Repair styling, make look more clean

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'landing#index'
+	authenticated :user do
+	  root 'posts#index', as: :authenticated_root
+	end
+	root 						  'landing#index'
   get  'resume'  => 'landing#resume'
   get  'about'   => 'landing#aboutme'
   get  'contact'   => 'landing#contact'
